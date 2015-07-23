726099043ced9689f237140e58c0315cfa3a0d06
taith <taisvbk@gmail.com>
2015-07-23 15:51:48 +0700

    add_bootstrap

--- src/framgiavn/project01/web/business/impl/UserBusinessImpl.java
+++ src/framgiavn/project01/web/business/impl/UserBusinessImpl.java
@@ -1,14 +1,13 @@
 package framgiavn.project01.web.business.impl;
 
 import org.hibernate.LockMode;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import framgiavn.project01.web.business.UserBusiness;
 import framgiavn.project01.web.dao.UserDAO;
 
 import framgiavn.project01.web.model.User;
 import framgiavn.project01.web.ulti.Logit2;
 
-public class UserBusinessImpl extends HibernateDaoSupport   implements UserBusiness {
+public class UserBusinessImpl  implements UserBusiness {
 	private static final Logit2 log = Logit2.getInstance(UserBusinessImpl.class);
 	private UserDAO userDAO;
 
@@ -31,13 +30,7 @@ public class UserBusinessImpl extends HibernateDaoSupport   implements UserBusin
 	}
 	@Override
 	public User findById(Integer id, boolean lock) throws Exception {
-		try {
-			//if (lock)
-				//getSession().set("User", LockMode.UPGRADE);
-			//getSession().lock("User", LockMode.UPGRADE);
-				
-			getHibernateTemplate().lock("User", LockMode.UPGRADE);
-						
+		try {						
 			return getUserDAO().findById(id);
 		} catch (Exception e) {
 			e.printStackTrace();
